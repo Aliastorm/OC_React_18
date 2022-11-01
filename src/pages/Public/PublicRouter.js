9eef8a40480ac319e3d54110400ae3802716408f
jfpons <jf.pons@gmail.com>
2022-11-01 13:27:48 +0100

    Router done

--- src/pages/Public/PublicRouter.js
+++ src/pages/Public/PublicRouter.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 
-import Layout from '@/pages/Public/Layout';
-import Home from '@/pages/Public/Home';
-import Service from '@/pages/Public/Service';
-import Contact from '@/pages/Public/Contact';
-import Error from '@/_utils/Error';
+import Layout from './Layout';
+import Home from './Home';
+import Service from './Service';
+import Contact from './Contact';
+import Error from '../../../src/_utils/Error';
 
 const PublicRoute = () => {
     return (
